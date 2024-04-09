d0bbfe356c4f9b92b159102c91774b3cf42b668b
CristianJR <104873760+CJRUIZ-WEBDESIGN@users.noreply.github.com>
2024-04-09 08:36:21 -0300

    Se ajustaron los margenes, se empieza con el responsive design, ajustando margenes, y navbar.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  site: 'https://cristianruiz.com.ar',
+  integrations: [react(), tailwind(), sitemap()],
+
 });
+
